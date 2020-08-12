36cc10f1b813c702fb0217bf8a6bfac27b06e200
Aung Ye Zaw <aungyezaw16316@gmail.com>
2020-08-12 11:10:52 +0630

    Update

--- AndroidStudio/NewsIoApi/app/src/main/java/com/aelayyuu/newsioapi/adapter/ItemAdapter.kt
+++ AndroidStudio/NewsIoApi/app/src/main/java/com/aelayyuu/newsioapi/adapter/ItemAdapter.kt
@@ -13,49 +13,46 @@ import kotlinx.android.synthetic.main.itemtopheader_layout.view.*
 
 class ItemAdapter (var itemlist : ArrayList<Article>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    var mClickListener : ItemViewHolder.ClickListener? = null
+    var mClickListener:ClickListener? = null
 
-    var setOnClickListener : ItemViewHolder.ClickListener? =null
+    var setOnClickListener:ClickListener? = null
 
-    fun setOnClickListener(clickListener: ItemViewHolder.ClickListener) {
+    fun setOnClickListener(clickListener:ClickListener) {
         this.mClickListener = clickListener
     }
-    fun updateArticle ( itemlist: ArrayList<Article>){
+
+    fun updateArticle(itemlist: ArrayList<Article>) {
         this.itemlist = itemlist
         notifyDataSetChanged()
     }
 
- inner class ItemViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
-    lateinit var article: Article
-    private var view:View = itemView
-
-    fun bind (article: Article) {
-        itemView.txtdetailtitle.text= article.title
-        itemView.txtdetailcontent.text = article.content
-        itemView.txtdetailauthor.text = article.author
-        itemView.txtdetaildescription.text = article.description
-        Picasso.get().load(article.urlToImage).into(itemView.imgitem)
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+        lateinit var article: Article
+        private var view: View = itemView
+        fun bind(article: Article) {
+            itemView.txtdetailtitle.text = article.title
+            itemView.txtdetailcontent.text = article.content
+            itemView.txtdetailauthor.text = article.author
+            itemView.txtdetaildescription.text = article.description
+            Picasso.get().load(article.urlToImage).into(itemView.imgitem)
+        }
+        override fun onClick(v: View?) {
+            mClickListener?.onClick(article)
+        }
     }
-
-    override fun onClick(v: View?) {
-        mClickListener?.onClick(article)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        var view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_detail_news, parent, false)
+        return ItemViewHolder(view)
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_detail_news,parent,false)
-        return DetailViewHolder(view)
-
-    }
-
     override fun getItemCount(): Int {
         return itemlist.size
-
     }
 
-    override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         holder.bind(itemlist[position])
     }
-
     interface ClickListener {
         fun onClick(article: Article)
     }
--- AndroidStudio/NewsIoApi/app/src/main/java/com/aelayyuu/newsioapi/fragments/TopHeadlineFragment.kt
+++ AndroidStudio/NewsIoApi/app/src/main/java/com/aelayyuu/newsioapi/fragments/TopHeadlineFragment.kt
@@ -1,27 +1,26 @@
 package com.aelayyuu.newsioapi.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aelayyuu.newsioapi.R
 import com.aelayyuu.newsioapi.adapter.ItemAdapter
-import com.aelayyuu.newsioapi.api.ApiClient
 import com.aelayyuu.newsioapi.model.Article
 import com.aelayyuu.newsioapi.model.News
 import com.aelayyuu.newsioapi.viewmodel.NewViewModel
 import kotlinx.android.synthetic.main.fragment_top_headline.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 var newsViewHolder = NewViewModel()
 
-class TopHeadlineFragment : Fragment() {
+class TopHeadlineFragment : Fragment() , ItemAdapter.ClickListener {
+    lateinit var newViewModel: NewViewModel
+    lateinit var itemAdapter: ItemAdapter
 
 
     override fun onCreateView(
@@ -34,39 +33,52 @@ class TopHeadlineFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        itemAdapter.setOnClickListener(this)
+
         newsViewHolder = ViewModelProvider(this).get(NewViewModel::class.java)
         newsViewHolder.loadResult()
+
         newsViewHolder.getResult().observe(viewLifecycleOwner, Observer {
+
             RecyclerView.apply {
-                layoutManager= LinearLayoutManager(context)
-                adapter=ItemAdapter(it.articles as ArrayList<Article>)
+                layoutManager = LinearLayoutManager(context)
+                adapter = ItemAdapter(it.articles as ArrayList<Article>)
 
             }
         })
+
+    }
+    fun observeViewmodel() {
+        newsViewHolder.getResult().observe(viewLifecycleOwner, Observer<News> { news ->
+            itemAdapter.updateArticle(news.articles as ArrayList<Article>)
+        })
         newsViewHolder.getloading().observe(
-            viewLifecycleOwner, Observer {
-                isLoading ->
-                if (isLoading)
-                {
+            viewLifecycleOwner, Observer { isLoading ->
+                if (isLoading) {
                     View.VISIBLE
-                }
-                else{
+                } else {
                     View.INVISIBLE
                 }
             }
         )
         newsViewHolder.geterrorStatus().observe(
-            viewLifecycleOwner,Observer {
-                status ->
-                if (status){
+            viewLifecycleOwner, Observer { status ->
+                if (status) {
                     newsViewHolder.geterrorMessage().observe(
-                        viewLifecycleOwner, Observer {
-                            message ->
+                        viewLifecycleOwner, Observer { message ->
                             txtResponse.text = message
                         }
                     )
                 }
             }
         )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        newsViewHolder.loadResult()
+    }
+    override fun onClick(article: Article) {
+        findNavController().navigate(R.id.action_topHeadlineFragment_to_detailNewsFragment)
     }
 }
