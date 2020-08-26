33163e4d14007285d94d1b48e47a3adf50f364a3
Aung Ye Zaw <aungyezaw16316@gmail.com>
2020-08-26 14:03:41 +0630

    Update

--- Flutter/flutter_3/lib/main.dart
+++ Flutter/flutter_3/lib/main.dart
@@ -1,56 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyStateApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget buttonSection = Container(
+      child: Row(
+        children: [_buildColumn(Icons.call, "CALL", color)],
+      ),
+    );
 
-     Widget buttonSection = Container(
-    child: Row(
-      children: [
-        _buildColumn(
-          Icons.call,
-          "CALL", color
-        )
-      ],
-    ),
-  );
-  
-
-final List<String> entries =['A','B','C'];
+    final List<String> entries = ['A', 'B', 'C'];
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Flutter"),
-          ),
-          // body: Row(children: [
-          //   Column(
-          //     children: [Icon(Icons.star), Text("12232")],
-          //   ),
-          //   Column(
-          //     children: [Icon(Icons.star), Text("12232")],
-          //   ),
-          //   _buildColumn(Icons.star, "3234", Colors.pink),
-          //   _buildColumn(Icons.file_download, "2433", Colors.red),
-          //   buttonSection
-          // ])),
-          body:ListView.builder(
-            itemCount: entries.length,
+        appBar: AppBar(
+          title: Text("Flutter"),
+        ),
+        // body: Row(children: [
+        //   Column(
+        //     children: [Icon(Icons.star), Text("12232")],
+        //   ),
+        //   Column(
+        //     children: [Icon(Icons.star), Text("12232")],
+        //   ),
+        //   _buildColumn(Icons.star, "3234", Colors.pink),
+        //   _buildColumn(Icons.file_download, "2433", Colors.red),
+        //       buttonSection
+
+        // ])),
+        // body: ListView.builder(
+        //   itemCount: entries.length,
+        //   itemBuilder: (BuildContext context, int index) {
+        //     return Container(
+        //       height: 50,
+        //       child: Center(
+        //         child: Text("Entry ${entries[index]}"),
+        //       )
+        //     );
+        //   },
+        // )
+        body: ListView.separated(
             itemBuilder: (BuildContext context, int index) {
               return Container(
                 height: 50,
-                child: Center(child: Text("Entry ${entries[index]}")
-                );
-                }
-          )
+                child: Center(
+                  child: Text("Entry ${entries[index]}"),
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) => Divider(),
+            itemCount: entries.length),
       ),
-    )
-    )
+    );
   }
 
   Color color = Colors.blue;
@@ -69,4 +76,95 @@ final List<String> entries =['A','B','C'];
       ],
     );
   }
+}
 
+class MyStateApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Stateful Widget"),
+        ),
+        body: _StatefulApp(),
+      ),
+    );
+  }
+}
+
+class _StatefulApp extends StatefulWidget {
+  @override
+  _StatefulAppState createState() => _StatefulAppState();
+}
+
+class _StatefulAppState extends State<_StatefulApp> {
+  int count = 0;
+
+  var text = 0;
+
+  final textName = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    void incrementCount() {
+      setState(() {
+        count++;
+      });
+    }
+
+    void decrementCount() {
+      setState(() {
+        count--;
+      });
+    }
+
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "$count",
+            style: TextStyle(fontSize: 50, color: Colors.blue),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                  onPressed: decrementCount,
+                  color: Colors.deepPurple,
+                  child: Text("Decrement",
+                      style: TextStyle(
+                        fontSize: 15,
+                      ))),
+                      SizedBox(width: 25,),
+              RaisedButton(
+                  onPressed: incrementCount,
+                  color: Colors.deepOrangeAccent,
+                  child: Text("Increasement",
+                      style: TextStyle(
+                        fontSize: 15,
+                      ))),
+            ],
+          ),
+          TextField(
+              controller: textName,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly
+              ],
+              decoration: InputDecoration(
+                  hintText: "Enter name", border: UnderlineInputBorder() )),
+          RaisedButton(
+              onPressed: () {
+                setState(() {
+                  text = int.parse(textName.text);
+                });
+              },
+              child: Text("Submit")),
+          Text("$text")
+        ],
+      ),
+    );
+  }
+}
