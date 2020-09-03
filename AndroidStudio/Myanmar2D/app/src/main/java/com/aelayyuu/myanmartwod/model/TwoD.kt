d9d80015fd69e45a78449df1c54621cfdb162ae1
Aung Ye Zaw <aungyezaw16316@gmail.com>
2020-09-03 16:15:03 +0630

    Update flutter 01/09/2020

--- AndroidStudio/Myanmar2D/app/src/main/java/com/aelayyuu/myanmartwod/model/TwoD.kt
+++
@@ -1,7 +0,0 @@
-package com.aelayyuu.myanmartwod.model
-
-data class TwoD(
-    val RESULT: String,
-    val SET: String,
-    val VALUE: String
-)
---
+++ AndroidStudio/Myanmar2D/app/src/main/java/com/aelayyuu/myanmartwod/model/TwoDresult.kt
@@ -0,0 +1,11 @@
+package com.aelayyuu.myanmartwod.model
+
+data class TwoDresult(
+    val RANDOM1: Int,
+    val RANDOM2: Int,
+    val RANDOM3: Int,
+    val RANDOM4: Int,
+    val RESULT: String,
+    val SET: String,
+    val VALUE: String
+)
